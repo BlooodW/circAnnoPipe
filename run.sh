e084fc4edb98614a1c53947687857ebf71a4e409
BlooodW <125759598+BlooodW@users.noreply.github.com>
2026-04-08 01:03:06 -0400

    Refactor run.sh for improved logging and clarity

--- run.sh
+++ run.sh
@@ -1,116 +1,130 @@
 #!/bin/bash
 
-# 设置日志文件
 LOG_FILE="run_log.txt"
 echo "Running Log - $(date)" > "$LOG_FILE"
 
-# 记录日志的函数
 log_message() {
     echo "$1" | tee -a "$LOG_FILE"
 }
 
-# 设置运行目录为当前工作目录
+# set direction
 RUN_DIR="$PWD/local"
 mkdir -p "$RUN_DIR"
 
-# 设置结果输出目录
 OUTPUT_DIR="$PWD/results"
 
-# 检查并创建目录（如果不存在的话）
 mkdir -p "$OUTPUT_DIR/miRNA"
 mkdir -p "$OUTPUT_DIR/RBP"
 mkdir -p "$OUTPUT_DIR/ORF"
 
-# 让用户输入 query 和 target 的 FASTA 文件路径
-read -p "请输入 circRNA 序列文件路径 (target)：" TARGET_FILE
-read -p "请输入 microRNA 序列文件路径 (query)：" QUERY_FILE
+read -p "please enter circRNA sequence file path (query) ：" TARGET_FILE
+read -p "Please enter the microRNA sequence file path (query)：" QUERY_FILE
 
-# 检查输入文件是否存在
 if [[ ! -f "$QUERY_FILE" || ! -f "$TARGET_FILE" ]]; then
-    log_message "错误: 输入文件不存在！"
+    log_message "ERROR: File doesn't exist！"
     exit 1
 fi
 
-# ======================== RNAhybrid 运行 ========================
+
+
+# ======================== RNAhybrid Run ========================
 run_RNAhybrid() {
-    log_message "运行 RNAhybrid..."
+    log_message "running RNAhybrid..."
     RNAhybrid -t "$TARGET_FILE" -q "$QUERY_FILE" -s 3utr_human > "$PWD/results/miRNA/hybrid.txt"
     
     if [[ $? -eq 0 ]]; then
-        log_message "RNAhybrid 运行完成，结果保存在 results/miRNA/hybrid.txt"
+        log_message "RNAhybrid running completed，saved to results/miRNA/hybrid.txt"
     else
-        log_message "错误: RNAhybrid 运行失败！"
+        log_message "ERROR: RNAhybrid failed！"
         exit 1
     fi
 
     python3 "$PWD/codes/miRNA/extract_RNAhybridRes.py"
 }
 
-# ======================== miRanda 运行 ========================
+# ======================== miRanda Run ========================
 run_miRanda() {
-    log_message "运行 miRanda..."
+    log_message "running miRanda..."
     "$PWD/packs/miranda/bin/miranda" "$QUERY_FILE" "$TARGET_FILE"  > "$PWD/results/miRNA/miRanda.txt"
     
     if [[ $? -eq 0 ]]; then
-        log_message "miRanda 运行完成，结果保存在 results/miRNA/miRanda.txt"
+        log_message "miRanda completed，saved to results/miRNA/miRanda.txt"
     else
-        log_message "错误: miRanda 运行失败！"
+        log_message "ERROR: miRanda failed！"
         exit 1
     fi
 
     python3 "$PWD/codes/miRNA/extract_miRandaRes.py"
 }
 
-# ======================== miRNA-circRNA 结果整合 ========================
+
+# ======================== TargetScan Run ========================
+run_TargetScan() {
+    log_message "running TargetScan..."
+    conda activate bio_env
+    mkdir $PWD/results/miRNA/targetscan/
+    python $PWD/codes/targetscan/generate_miR_family_info.py "$QUERY_FILE" -o $PWD/results/miRNA/targetscan/miR_Family_info.txt
+    python $PWD/codes/targetscan/generate_circRNA_input.py "$TARGET_FILE" -o $PWD/results/miRNA/targetscan/circRNA_input.txt
+
+    
+    perl $PWD/packs/targetscan/targetscan_70.pl \
+        $PWD/results/miRNA/targetscan/miR_Family_info.txt \
+        $PWD/results/miRNA/targetscan/circRNA_input.txt \
+        $PWD/results/miRNA/targetscan/raw_output.txt
+    
+    conda deactivate
+    cd ../../../
+
+    if [[ $? -eq 0 ]]; then
+        log_message "TargetScan running completed."
+    else
+        log_message "ERROR: TargetScan failed！"
+        exit 1
+    fi
+
+}
+
+# ======================== miRNA-circRNA Result Integration ========================
 RNAhybrid_miRanda_merge() {
-    log_message "合并 RNAhybrid 和 miRanda 结果..."
-    python3 "$PWD/codes/miRNA/RNAhybrid_miRanda_merge.py"
+    log_message "merging RNAhybrid 和 miRanda results..."
+    python3 "$PWD/codes/miRNA/miRNA_num.py"
     
     if [[ $? -eq 0 ]]; then
-        log_message "结果整合完成"
+        log_message "Merge completed"
     else
-        log_message "错误: 结果整合失败！"
+        log_message "ERROR: merge failed！"
         exit 1
     fi
 }
 
-
-# ======================== beRBP 运行 ========================
+# ======================== beRBP Run ========================
 run_beRBP() {
-    # 复制目标文件
     cp "$TARGET_FILE" "$PWD/packs/beRBP/work/temp/001.fasta"
 
-    # 切换到 beRBP 代码目录
     cd "$PWD/packs/beRBP/code/" || { echo "Error: Failed to enter directory $PWD/beRBP/code/"; return 1; }
 
-    # 确保 core_csrv.sh 存在
     if [[ ! -f core_csrv.sh ]]; then
         echo "Error: core_csrv.sh not found!"
         return 1
     fi
 
-    # 备份 core_csrv.sh 以防修改出错
     cp core_csrv.sh core_csrv.sh.bak
 
-    # 修改 core_csrv.sh 的 blastn 语句
     python core_csrv_mod.py
     
     cd ../work
 
     mkdir -p temp
 
-    # 运行 general
     ../code/general_sPWM.sh 001 "all" "all" > temp/001.log &
 
-    # 等待任务完成
     wait
 
-    # 移动
     if [[ -f 001/resultMatrix.tsv ]]; then
         cp 001/resultMatrix.tsv ../../../results/RBP/beRBP.tsv
         echo "Moved resultMatrix.tsv to /results/RBP/beRBP.tsv"
     else
-        echo "Error: resultMatrix.tsv not found!"
+        echo "ERROR: resultMatrix.tsv not found!"
         return 1
     fi
 
@@ -119,7 +133,7 @@ run_beRBP() {
     cd ../../../
 }
 
-# ======================== ENCORI 运行 ========================
+# ======================== ENCORI Run ========================
 run_ENCORI() {
     mkdir -p "$PWD/results/RBP"
     echo "Downloading ENCORI RBP-Target data for circRNAs..."
@@ -128,18 +142,15 @@ run_ENCORI() {
     echo "Download completed. Data saved to $PWD/results/RBP/ENCORI_hg38_RBPTarget_all_circRNA_HeLa.txt"
 }
 
-# ======================== RBP-circRNA 结果整合 ========================
+# ======================== RBP-circRNA Results Integration ========================
 beRBP_ENCORI_merge() {
 
-    # 定义输入文件路径
     ENC_FILE="$PWD/results/RBP/ENCORI_hg38_RBPTarget_all_circRNA_HeLa.txt"
     RBP_FILE="$PWD/results/RBP/beRBP.tsv"
     OUTPUT_FILE="$PWD/results/RBP/merged_output.txt"
 
-    # 执行 Python 合并脚本
     python "$PWD/codes/RBP/beRBP_ENCORI_merge.py" "$ENC_FILE" "$RBP_FILE" "$OUTPUT_FILE"
 
-    # 检查是否成功生成合并文件
     if [[ -f "$OUTPUT_FILE" ]]; then
         echo "Merge completed successfully. Output saved to $OUTPUT_FILE"
     else
@@ -149,15 +160,13 @@ beRBP_ENCORI_merge() {
 }
 
 
-# ======================== IRESfinder 运行 ========================
+# ======================== IRESfinder Run ========================
 run_IRESfinder() {
-    log_message "IRESfinder 运行中..."
+    log_message "IRESfinder running..."
 
-    # 检查 IRESfinder.py 第 47 行的缩进
     log_message "Checking indentation of line 47 in IRESfinder.py..."
     IRES_PY="$PWD/packs/IRESfinder/IRESfinder.py"
 
-    # 读取第 47 行的前缀字符
     LINE_47=$(sed -n '47p' "$IRES_PY")
     PREFIX=$(echo "$LINE_47" | sed -E 's/[^ ].*//') 
 
@@ -168,44 +177,41 @@ run_IRESfinder() {
         sed -i '47s/^[ \t]*/        /' "$IRES_PY"
     fi
 
-    # 激活 Python 2.7 conda 环境
     log_message "Activating Python 2.7 environment..."
     source activate py27_env || { log_message "Error: Conda environment activation failed."; exit 1; }
 
-    # 运行 IRESfinder
     python "$IRES_PY" -f "$TARGET_FILE" -o "$PWD/results/ORF/IRESfinder_mode_1.result" -m 1
     python "$IRES_PY" -f "$TARGET_FILE" -o "$PWD/results/ORF/IRESfinder_mode_2.result" -m 2 -w 174 -s 50
 
-    # 检查运行是否成功
     if [[ $? -eq 0 ]]; then
-        log_message "IRESfinder 运行完成"
+        log_message "IRESfinder completed"
     else
-        log_message "错误: IRESfinder 运行失败！"
+        log_message "ERROR: IRESfinder failed！"
         exit 1
     fi
 
-    # 退出 conda 环境
     log_message "Deactivating conda environment..."
     conda deactivate
 }
 
-# ======================== ORFfinder 运行 ========================
+# ======================== ORFfinder Run ========================
 run_ORFfinder() {
-    log_message "ORFfinder 运行中..."
+    log_message "ORFfinder running..."
     $PWD/packs/ORFfinder -in "$TARGET_FILE" -out "$PWD/results/ORF/ORFfinder.fa"
     
     if [[ $? -eq 0 ]]; then
-        log_message "ORFfinder 运行完成"
+        log_message "ORFfinder completed"
     else
-        log_message "错误: ORFfinder 运行失败！"
+        log_message "ERROR: ORFfinder failed！"
         exit 1
     fi
 }
 
-# ======================== 主函数 ========================
+# ======================== main ========================
 main() {
     run_RNAhybrid
     run_miRanda
+    run_TargetScan
     RNAhybrid_miRanda_merge
     run_beRBP
     run_ENCORI
@@ -214,5 +220,5 @@ main() {
     run_ORFfinder
 }
 
-# 执行主函数
+# execute main
 main

375ffd079f0c12c6be4fbe1ebc9aa9a5776defd4
BlooodW <125759598+BlooodW@users.noreply.github.com>
2026-04-12 00:16:28 -0400

    Fix missing function call in convert_faToSeedSeq.py

--- codes/miRNA/convert_faToSeedSeq.py
+++ codes/miRNA/convert_faToSeedSeq.py
@@ -4,16 +4,12 @@ def convert_miRNA_format(input_file, output_file):
     with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
         for line in infile:
             if line.startswith('>'):
-                # 提取 miRNA 名称、miRBase ID 和物种信息
                 parts = line[1:].strip().split(' ')
                 name = parts[0]
                 miRBase_id = "9606"
             else:
-                # 提取成熟序列
                 mature_sequence = line.strip()
-                # 提取种子序列（第 2-8 位）
                 seed_sequence = mature_sequence[1:8]
-                # 写入输出文件
                 outfile.write(f"{name}\t{seed_sequence}\t{miRBase_id}\n")
 
 if __name__ == "__main__":
